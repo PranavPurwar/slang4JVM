7572aaca03499481fc648337e34deb2b50d4f186
Sarath Soman <ssoman@applieddatafinance.com>
2016-03-15 00:47:11 +0530

    Gitignore added, Main modified to read test files from resource

--- sang-bytecode-generation/src/main/java/com/slang/main/Main.java
+++ sang-bytecode-generation/src/main/java/com/slang/main/Main.java
@@ -1,46 +1,40 @@
 package com.slang.main;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import com.slang.main.Slang;
+import java.io.InputStreamReader;
 //TODO Separate to different project later
 public class Main {
-	static String readFile(File file) {
+	static String readFile(String resource) {
 		String content = null;
-		FileReader reader = null;
-		try {
-			reader = new FileReader(file);
-			char[] chars = new char[(int) file.length()];
-			reader.read(chars);
-			content = new String(chars);
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+				Thread.currentThread().getContextClassLoader().getResourceAsStream(resource)));){
+			String line = null;
+			StringBuilder sb = new StringBuilder();
+			while(null !=(line = reader.readLine())) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+			}
+			content = sb.toString();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		} 
 		return content;
 	}
 
 	public static void main(String[] args) {
 		String[] scriptArray = new String[] {
-				"resources/test_scripts/0_discriminant.sl",
-				"resources/test_scripts/1_factorial.sl",
-				"resources/test_scripts/2_fibonacci1.sl",
-				"resources/test_scripts/3_fibonacci2.sl",
-				"resources/test_scripts/4_fibonacci3.sl",
-				"resources/test_scripts/5_HelloWorld.sl",
-				"resources/test_scripts/6_oneToHundred.sl" };
+				"0_discriminant.sl",
+				"1_factorial.sl",
+				"2_fibonacci1.sl",
+				"3_fibonacci2.sl",
+				"4_fibonacci3.sl",
+				"5_HelloWorld.sl",
+				"6_oneToHundred.sl" };
 		try {
-			File file = new File(scriptArray[4]);
-			String source = readFile(file);
-			String name = file.getName().split("\\.")[0].split("_")[1];
+			String source = readFile(scriptArray[4]);
+			String name = scriptArray[4];
 			Slang sl = new Slang(name, source);
 			sl.interpret();
 			sl.compile();
